db912b1ede122c13948506b87450d6066fe8f7e3
Furkan Sarı <furkansarip@gmail.com>
2023-04-04 16:39:20 +0300

    Change appicon & Programmatic UI Setup

---
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Furkan Sarı on 4.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
